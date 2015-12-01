091318946f1c395615c8b3d3611a916d6d64c5a8
AAAleg <awwert@yandex.ru>
2015-12-01 18:05:11 +0900

    Adding user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+end
