d5c98625a90d51fbbd6cada39c1221f83237ce8d
AAAleg <awwert@yandex.ru>
2015-12-03 17:50:59 +0900

    Adding styles for layouts and user pages. Add Factory girl. Add Gravatar

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -39,4 +39,12 @@ RSpec.describe "Users", type: :request do
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
