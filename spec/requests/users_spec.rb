3a6568aedc71db196addf7bf55ada6fb7ca84e32
AAAleg <awwert@yandex.ru>
2015-12-04 12:52:51 +0900

    Create signup form

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,6 +9,27 @@ RSpec.describe "Users", type: :request do
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+
+    let(:submit){ "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "example@test.ru"
+        fill_in "Password", with: "master"
+        fill_in "Confirmation", with: "master"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count)
+      end
+    end
   end
 
   describe "profile page" do
