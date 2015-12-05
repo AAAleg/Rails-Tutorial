67f2710b6091c8532a956d79004ea363fe962fc4
AAAleg <awwert@yandex.ru>
2015-12-05 18:36:32 +0600

    Finish sign-in-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+/i
 
@@ -10,4 +11,19 @@ class User < ActiveRecord::Base
              uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
+  class << self
+    def new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -29,6 +29,15 @@ RSpec.describe "Users", type: :request do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'example@test.ru') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
 
